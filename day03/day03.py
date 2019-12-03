3708456d8eb727dd0a6e222d7cb187a9b02fa436
Marco Zietzling <marco.zietzling@gmail.com>
2019-12-03 22:39:10 +0100

    day 3 part 1

--- day03/day03.py
+++ day03/day03.py
@@ -1,57 +1,58 @@
 print("advent of code 2019 - day 3")
 
+# day 1 - part 1
+
 with open("input.txt") as file:
     lines = [line.split(",") for line in file]
 
-grid = [[' ' for x in range(-13000, 13000)] for y in range(-13000, 13000)]
 
-
-def print_grid(grid):
-    with open("result1.txt", mode="w") as file:
-        file.write("\n".join([''.join(["{:1}".format(i) for i in row]) for row in grid]))
-        # for y in range(len(grid)):
-        #     for x in range(len(grid)):
-        #         file.write("{:1}".format(grid[x][y]))
-        #
-        #     file.write("\n")
+def calculate_manhatten_distance(position):
+    return abs(position[0]) + abs(position[1])
 
 
 def process_wire(wire):
     x = 0
     y = 0
-    grid[y][x] = "O"
+    wire_positions = set()
+
     for action in wire:
         direction = action[:1]
         steps = int(action[1:])
-        print(f"go {direction} {steps} steps")
+        # print(f"go {direction} {steps} steps")
+
         if direction == "R":
-            for x in range(x + 1, x + steps, +1):
-                grid[y][x] = "-"
-
-            x = x + 1
+            for x in range(x + 1, x + steps + 1, +1):
+                wire_positions.add((x, y))
         elif direction == "L":
-            for x in range(x - 1, x - steps, -1):
-                grid[y][x] = "-"
-
-            x = x - 1
+            for x in range(x - 1, x - steps - 1, -1):
+                wire_positions.add((x, y))
         elif direction == "U":
-            for y in range(y + 1, y + steps, +1):
-                grid[y][x] = "|"
-
-            y = y + 1
+            for y in range(y + 1, y + steps + 1, +1):
+                wire_positions.add((x, y))
         elif direction == "D":
-            for y in range(y - 1, y - steps, -1):
-                grid[y][x] = "|"
-
-            y = y - 1
+            for y in range(y - 1, y - steps - 1, -1):
+                wire_positions.add((x, y))
         else:
             print(f"unknown direction {direction}")
 
-        grid[y][x] = "X"
-        print(f"current coordinates {x} {y} and {grid[y][x]}")
+        # print(f"current coordinates {x} {y}")
+
+    return wire_positions
 
 
-process_wire(lines[0])
-# process_wire(lines[1])
+wire1_positions = process_wire(lines[0])
+wire2_positions = process_wire(lines[1])
 
-print_grid(grid)
+crossings = wire1_positions.intersection(wire2_positions)
+
+min_distance = 99999
+for crossing in crossings:
+    distance = calculate_manhatten_distance(crossing)
+    if distance < min_distance:
+        min_distance = distance
+
+# result = 1195
+print(f"minimum manhattan distance = {min_distance}")
+
+# day 3 - part 2
+
