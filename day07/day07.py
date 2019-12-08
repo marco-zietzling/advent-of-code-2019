764d3f87be4da19972063100e9e43c0e3fc77f4f
Marco Zietzling <marco.zietzling@gmail.com>
2019-12-08 01:18:42 +0100

    day 7 part 2 (try)

--- day07/day07.py
+++ day07/day07.py
@@ -20,9 +20,8 @@ def get_parameter(mode: int, program: list, param: int):
         return int(param)
 
 
-def run_program(input_program: list, phase_setting: int, input_signal: int):
-    index = 0
-    program = input_program.copy()
+def run_program(program: list, phase_setting: int, input_signal: int, starting_index: int = 0):
+    index = starting_index
     phase_setting_used = False
     input_signal_used = False
     program_output = 9999
@@ -41,7 +40,7 @@ def run_program(input_program: list, phase_setting: int, input_signal: int):
 
         # halt (and return program output)
         if opcode == 99:
-            return program_output
+            return (99, index, program_output)
 
         # addition
         elif opcode == 1:
@@ -63,11 +62,11 @@ def run_program(input_program: list, phase_setting: int, input_signal: int):
         elif opcode == 3:
             target_index = program[index + 1]
             # input_to_be_used = input("Enter your input: ")
-            if phase_setting_used == False:
+            if not phase_setting_used:
                 # print(f"phase setting = {phase_setting}")
                 input_to_be_used = phase_setting
                 phase_setting_used = True
-            elif input_signal_used == False:
+            elif not input_signal_used:
                 # print(f"input signal = {input_signal}")
                 input_to_be_used = input_signal
                 input_signal_used = True
@@ -85,6 +84,7 @@ def run_program(input_program: list, phase_setting: int, input_signal: int):
             program_output = program[target_index]
             index = index + 2
             # print(f"program output = {program[target_index]}")
+            return (4, index, program_output)
 
         # jump if true
         elif opcode == 5:
@@ -136,9 +136,9 @@ def run_program(input_program: list, phase_setting: int, input_signal: int):
             break
 
 
-def run_amplifier(input_program: list, amp_name: str, phase_setting: int, input_signal: int):
+def run_amplifier(input_program: list, amp_name: str, phase_setting: int, input_signal: int, starting_index: int = 0):
     # print(f"running amplifier {amp_name} with phase setting = {phase_setting} and input signal = {input_signal}")
-    output = run_program(input_program, phase_setting, input_signal)
+    output = run_program(input_program, phase_setting, input_signal, starting_index)
     # print(f"result of amplifier {amp_name} = {output}")
     return output
 
@@ -149,11 +149,11 @@ phase_settings = [0, 1, 2, 3, 4]
 max_thruster = -999
 
 for permutation in itertools.permutations(phase_settings, 5):
-    result_amp_a = run_amplifier(program, "A", phase_setting=permutation[0], input_signal=0)
-    result_amp_b = run_amplifier(program, "B", phase_setting=permutation[1], input_signal=result_amp_a)
-    result_amp_c = run_amplifier(program, "C", phase_setting=permutation[2], input_signal=result_amp_b)
-    result_amp_d = run_amplifier(program, "D", phase_setting=permutation[3], input_signal=result_amp_c)
-    result_amp_e = run_amplifier(program, "E", phase_setting=permutation[4], input_signal=result_amp_d)
+    (_, _, result_amp_a) = run_amplifier(program.copy(), "A", phase_setting=permutation[0], input_signal=0)
+    (_, _, result_amp_b) = run_amplifier(program.copy(), "B", phase_setting=permutation[1], input_signal=result_amp_a)
+    (_, _, result_amp_c) = run_amplifier(program.copy(), "C", phase_setting=permutation[2], input_signal=result_amp_b)
+    (_, _, result_amp_d) = run_amplifier(program.copy(), "D", phase_setting=permutation[3], input_signal=result_amp_c)
+    (_, _, result_amp_e) = run_amplifier(program.copy(), "E", phase_setting=permutation[4], input_signal=result_amp_d)
 
     if result_amp_e > max_thruster:
         max_thruster = result_amp_e
@@ -162,3 +162,41 @@ for permutation in itertools.permutations(phase_settings, 5):
 print(f"max thruster result = {max_thruster}")
 
 # day 7 - part 2
+phase_settings = [5, 6, 7, 8, 9]
+
+# for permutation in itertools.permutations(phase_settings, 5):
+
+result_amp_e = 0  # initial input for amp A is 0
+index_a = 0
+program_a = program.copy()
+index_b = 0
+program_b = program.copy()
+index_c = 0
+program_c = program.copy()
+index_d = 0
+program_d = program.copy()
+index_e = 0
+program_e = program.copy()
+
+permutation = [9, 8, 7, 6, 5]
+while True:
+    (_, index_a, result_amp_a) = run_amplifier(program_a, "A", phase_setting=permutation[0], input_signal=result_amp_e,
+                                               starting_index=index_a)
+    (_, index_b, result_amp_b) = run_amplifier(program_b, "B", phase_setting=permutation[1], input_signal=result_amp_a,
+                                               starting_index=index_b)
+    (_, index_c, result_amp_c) = run_amplifier(program_c, "C", phase_setting=permutation[2], input_signal=result_amp_b,
+                                               starting_index=index_c)
+    (_, index_d, result_amp_d) = run_amplifier(program_d, "D", phase_setting=permutation[3], input_signal=result_amp_c,
+                                               starting_index=index_d)
+    (return_code_e, index_e, result_amp_e) = run_amplifier(program_e, "E", phase_setting=permutation[4],
+                                                           input_signal=result_amp_d, starting_index=index_e)
+
+    print(f"return code amp E = {return_code_e} with result = {result_amp_e}")
+
+    # check for halt
+    if return_code_e == 99:
+        print(f"max thruster result after feedback = {result_amp_e}")
+        break
+
+# if result_amp_e > max_thruster:
+#     max_thruster = result_amp_e
