1446eec0f5cc2571680cd2404be97cce05c14295
Marco Zietzling <marco.zietzling@gmail.com>
2019-12-12 13:38:49 +0100

    refactored day 5 to new intcode computer

--- day05/day05.py
+++ day05/day05.py
@@ -7,12 +7,16 @@ print("advent of code 2019 - day 5")
 input_file = os.path.join(os.getcwd(), "input.txt")
 
 computer = comp.IntcodeComputer("Day 5", input_file)
-result = computer.run_program([1])
+computer.provide_arguments([1])
+computer.execute()
+result = computer.consume_output()
 print(f"result for part 1 = {result}")
 # result = 7286649
 
 # day 5 - part 2
 computer = comp.IntcodeComputer("Day 5", input_file)
-result = computer.run_program([5])
+computer.provide_arguments([5])
+computer.execute()
+result = computer.consume_output()
 print(f"result for part 2 = {result}")
 # result = 15724522
